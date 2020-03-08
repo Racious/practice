00f6c252907d1a38e24f2bc0a7cb93af8ccf71e3
Racious <awenracious@gmail.com>
2020-03-08 23:57:12 +0900

    猜數字幾A幾B 精簡化

--- src/Guess/Guess.java
+++ src/Guess/Guess.java
@@ -2,10 +2,50 @@ package Guess;
 
 import java.util.Random;
 import java.util.Scanner;
-
 //幾A幾B 猜輸字程式
+
 public class Guess {
     public static void main(String[] args) {
+        //產生陣列放置隨機亂數
+        int ansseat[] = gamestart();
+        //good bad 等於A B數量
+        int good;
+        int bad;
+        Scanner req = new Scanner(System.in);
+        while (req.hasNext()) {
+            //建立字串 s，將輸入的數字放入
+            String s = req.nextLine();
+            int[] ary = new int[s.length()]; // 宣告一個整數陣列，長度為字串 s 的長度
+            //判斷輸入的長度是否為4
+            switch (ary.length) {
+                case 4:
+                    for (int i = 0; i < s.length(); i++) { // 利用迴圈，依序讀取字串 s 中的每一個字元
+                        String s2 = s.substring(i, i + 1); // 利用 substring() 來捉每一個字元放入字串 s2
+                        ary[i] = Integer.parseInt(s2); // 將捉到的字元轉為整數，存入陣列
+                    }
+                    //判斷輸入的字是否有重複
+                    if (check(ary)) {
+                        System.out.println("輸入的字有重複唷！");
+                    } else {
+                        good = anwser(ary, ansseat);
+                        if (good == 4) {
+                            System.out.println("恭喜猜對了！！");
+                            req.close();
+                            return;
+                        } else {
+                            System.out.println("好可惜 再試試看");
+                        }
+                    }
+                    break;
+                //若輸入的數字不是4個
+                default:
+                    System.out.println("輸入的長度不對唷！");
+                    break;
+            }
+        }
+    }
+
+    public static int[] gamestart() {
         //產生隨機數字
         int ansseat[] = new int[4];
         //產生亂數名為ans
@@ -25,58 +65,33 @@ public class Guess {
         //亂數的答案
         System.out.println("答案:" + ansseat[0] + ansseat[1] + ansseat[2] + ansseat[3]);
         System.out.println("\n");        //輸入數字
-        //GOOD Bad 等於A B數量
-        int Good;
-        int Bad;
-        Scanner req = new Scanner(System.in);
-        while (req.hasNext()) {
-            //建立字串 s，將輸入的數字放入
-            String s = req.nextLine();
-            int[] ary = new int[s.length()]; // 宣告一個整數陣列，長度為字串 s 的長度
-            //判斷輸入的長度是否為4
-            switch (ary.length) {
-                case 4:
-                    for (int i = 0; i < s.length(); i++) { // 利用迴圈，依序讀取字串 s 中的每一個字元
-                        String s2 = s.substring(i, i + 1); // 利用 substring() 來捉每一個字元放入字串 s2
-                        ary[i] = Integer.parseInt(s2); // 將捉到的字元轉為整數，存入陣列
+        return ansseat;
+    }
+
+    public static boolean check(int[] ary) {
+        return ary[0] == ary[1] || ary[0] == ary[2] || ary[0] == ary[3] || ary[1] == ary[2] || ary[1] == ary[3] || ary[2] == ary[3];
+    }
+
+    public static int anwser(int[] ary, int[] ansseat) {
+        //重置AB數
+        int good = 0;
+        int bad = 0;
+        //判斷AB數量
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                    if (ary[i] == ansseat[j]) {
+                        good++;
                     }
-                    //判斷輸入的字是否有重複
-                    if (ary[0] == ary[1] || ary[0] == ary[2] || ary[0] == ary[3] || ary[1] == ary[2] || ary[1] == ary[3] || ary[2] == ary[3]) {
-                        System.out.println("輸入的字有重複唷！");
-                    } else {
-                            //重置AB數
-                            Good = 0;
-                            Bad = 0;
-                            //判斷AB數量
-                            for (int i = 0; i < 4; i++) {
-                                for (int j = 0; j < 4; j++) {
-                                    if (j == i) {
-                                        if (ary[i] == ansseat[j]) {
-                                            Good = Good + 1;
-                                        }
-                                    } else if (ary[i] == ansseat[j]) {
-                                        Bad = Bad + 1;
-                                    }
-                                }
-                            }
-                            //輸出顯示幾A幾B
-                            System.out.println(Good + "A" + Bad + "B");
-                            //若4A 結束遊戲
-                            if (Good == 4) {
-                                System.out.println("恭喜猜對了！！");
-                                req.close();
-                                return;
-                            } else {
-                                System.out.println("好可惜 再試試看");
-                            }
-//                        }
-                    }
-                    break;
-                //若輸入的數字不是4個
-                default:
-                    System.out.println("輸入的長度不對唷！");
-                    break;
+                } else if (ary[i] == ansseat[j]) {
+                    bad++;
+                }
             }
         }
+        //輸出顯示幾A幾B
+        System.out.println(good + "A" + bad + "B");
+        return good;
+        //若4A 結束遊戲
     }
+
 }
