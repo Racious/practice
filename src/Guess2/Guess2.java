7a6c9fa03d8d86bf32a8b097bb5c03b015ce1be8
Racious <awenracious@gmail.com>
2020-03-09 12:05:01 +0900

    簡易猜數字，將計算分離

--- src/Guess2/Guess2.java
+++ src/Guess2/Guess2.java
@@ -10,26 +10,17 @@ public class Guess2 {
         //產生亂數0~99
         Random random = new Random();
         int x = random.nextInt(100);
-        int max=99;
-        int min=0;
         //答案
         //System.out.println(x);
+
         //請求輸入
         Scanner req = new Scanner(System.in);
-        while (req.hasNext())
-        {
+        calc ans = new calc();
+        while (req.hasNext()) {
             //判斷大小
             int y = req.nextInt();
-            if (y > x) {
-                System.out.println("數字太大囉，再小一點");
-                max=y;
-                System.out.println("請猜"+min+"~"+max+"的數字");
-            } else if (y < x) {
-                System.out.println("數字太小囉，再大一點");
-                min=y;
-                System.out.println("請猜"+min+"~"+max+"的數字");
-            } else if (x==y) {
-                System.out.println("猜對了，真厲害");
+            //將亂數值x及猜測值y後送play方法計算，並傳回布林值
+            if (ans.play(y, x)) {
                 return;
             }
         }
---
+++ src/Guess2/calc.java
@@ -0,0 +1,28 @@
+package Guess2;
+
+public class calc {
+        int max=99;
+        int min=0;
+
+        public  boolean play(int y,int x){
+            boolean z= false;
+        if (y > x) {
+            System.out.println("數字太大囉，再小一點");
+            if(max>y){
+                max=y;
+            }
+            System.out.println("請猜"+min+"~"+max+"的數字");
+        } else if (y < x) {
+            System.out.println("數字太小囉，再大一點");
+            if(min<y){
+                min=y;
+            }
+            System.out.println("請猜"+min+"~"+max+"的數字");
+        } else if (x==y) {
+            System.out.println("猜對了，真厲害");
+            //表示猜對，可結束程式
+            z = true;
+        }
+        return z;
+    }
+}
