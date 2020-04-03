031238d4842f407c581f4ef22233d51258d1fcb0
Racious <awenracious@gmail.com>
2020-04-03 20:56:19 +0900

    奶茶

--- src/語法練習/OOP/Beverage/Milk_Tea.java
+++ src/語法練習/OOP/Beverage/Milk_Tea.java
@@ -2,7 +2,12 @@ package Beverage;
 
 public class Milk_Tea extends Beverage {
     public Milk_Tea() {
-        setName("奶茶");
-        setCost(30);
+    }
+    public String getName() {
+        return "奶茶";
+    }
+
+    public double getCost(){
+        return 30;
     }
 }
