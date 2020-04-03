90a610e3c1ca2eed828cf9f4771835698762a987
Racious <awenracious@gmail.com>
2020-04-03 14:44:33 +0900

    角色裝備武器

--- src/語法練習/OOP/Kingdom/Character.java
+++ src/語法練習/OOP/Kingdom/Character.java
@@ -3,7 +3,7 @@ package Kingdom;
 public abstract class Character {
     private String name;//角色名子
     private int attack;//角色攻擊力
-    Weapon weapon=new Empty_hand();//建立抽象介面，並預設空手攻擊模式
+    Weapon weapon=new Empty_hand();//透過介面宣告變數，並預設空手攻擊模式
 
     public String getName() {
         return name;
